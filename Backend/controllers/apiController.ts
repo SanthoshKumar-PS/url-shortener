895d2c3fa130d392044dce6fbeaa25214145bea6
SanthoshKumar-PS <santhoshkumarsakthi2003@gmail.com>
2025-11-27 18:10:46 +0530

    Dashboard completed with pagination

--- Backend/controllers/apiController.ts
+++ Backend/controllers/apiController.ts
@@ -22,25 +22,36 @@ export const createShortUrl = async (req:any,res:any) => {
     }   
 }
 
-export const getAllData = async (req:any,res:any) => {
+export const getAllData = async (req:any,res:any) => {  
     try {
-        const {pageNo, limit, search} = req.params;
-        const searchParam = search.toLowerCase();
-        const totalSkip = (pageNo-1)*limit
-        const whereClause:Prisma.UrlShortenerWhereInput ={
+        const {pageNo, limit, search} = req.query;
+        const page = Number(pageNo)||1
+        const pageLimit = Number(limit)||10
+        const searchParam = search ? String(search).toLowerCase() : ""
+        const totalSkip = (page-1)*pageLimit
+        console.log({page:page,limit:pageLimit,search:searchParam})
+        const whereClause:Prisma.UrlShortenerWhereInput =(searchParam && searchParam.length>0) ? {
             OR:[
-                {longUrl : {contains:searchParam}},
-                {shortCode : {contains:searchParam}},
+                {longUrl : {contains:searchParam, mode:'insensitive'}},
+                {shortCode : {contains:searchParam, mode:'insensitive'}},
             ]
-        }
+        } : {}
+        console.log(whereClause)
         const results = await prisma.urlShortener.findMany({
             where:whereClause,
             skip:totalSkip,
-            take:limit
+            take:pageLimit,
+            orderBy:{
+                createdAt:'desc'
+            }
+        });
+        const totalDataCount = await prisma.urlShortener.count({
+            where:whereClause
         });
 
+
         console.log("Data retrieved: ",results)
-        return res.status(200).json({message:"Fetched Successfully",results:results})
+        return res.status(200).json({message:"Fetched Successfully",results:results,totalDataCount, totalPages: Math.ceil(totalDataCount/pageLimit) })
 
     } catch (error:any) {
         console.log("Error occured while fetching data: ",error.message)
@@ -58,9 +69,18 @@ export const getStatsData = async (req:any,res:any) => {
 
 export const deleteData = async (req:any,res:any) => {
     try {
-        
+        const {id} = req.query;
+        console.log("id to delete: ",id);
+        const deletedData = await prisma.urlShortener.delete({
+            where:{
+                id:Number(id)
+            }
+        })
+        console.log("Deleted successfully")
+        return res.status(200).json({message:"Deleted successfully"})
     } catch (error:any) {
-        
+        console.log("Error occured while deleting data: ",error.message);
+        return res.status(500).json({message:"Internal Server Error"})
     }   
 }
 
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -12,14 +12,21 @@ import Pagination from '@/components/Pagination';
 import { useDebounce } from '@/components/DebounceSearch';
 import axios from 'axios';
 import type { UrlShortenerType } from '@/types/TableTypes';
+import { div } from 'framer-motion/client';
+import { Spinner } from '@/components/Spinner';
+import { useNavigate } from 'react-router-dom';
+import {motion} from 'framer-motion'
+import { Trash2 } from 'lucide-react';
+
 const Dashboard = () => {
     const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
     const [isLoading, setIsLoading] = useState<boolean>(false);
-    const [data,setData] = useState<UrlShortenerType[]>([])
+    const navigate = useNavigate();
+    const [datas,setDatas] = useState<UrlShortenerType[]>([])
 
     const [searchParams, setSearchParams] = useState<string>("");
     const debounceValue = useDebounce(searchParams,500);
-    const [totalOrdersCount,setTotalOrdersCount] = useState<number>(0);
+    const [totalDataCount,setTotaDataCount] = useState<number>(0);
     const [pageNo,setPageNo] = useState<number>(1);
     const [totalPages, setTotalPages] = useState<number>(1);
     const [limit,setLimit] = useState<number>(5);
@@ -35,6 +42,9 @@ const Dashboard = () => {
             }
         });
         console.log("Response recieved: ",response.data)
+        setDatas(response.data.results)
+        setTotaDataCount(response.data.totalDataCount)
+        setTotalPages(response.data.totalPages)
     } catch (error:any) {
         console.log("Error occured while fetching data")
         //TODO: Toast
@@ -43,9 +53,27 @@ const Dashboard = () => {
     }
   }
 
-  useEffect(()=>{
+  const handleDeleteData = async (id:number) => {
+    try {
+        setIsLoading(true)
+        const response = await axios.delete(`${BACKEND_URL}/api/links`,{
+            params:{
+                id
+            }
+        })
+        console.log("Deletion successful");
+        handleGetAllData()
+    } catch (error:any) {
+        console.log("Error occured while deleting a data: ",error.message);
+        //TODO: Toast Error
+    } finally{
+        setIsLoading(false);
+    }
+  }
 
-  },[pageNo, limit, ])
+  useEffect(()=>{
+    handleGetAllData()
+  },[pageNo, limit, debounceValue])
 
   return (
     <div className='min-h-screen w-full bg-gradient-to-br from-blue-100/30 via-blue-200/70 to-blue-100/50 p-4 md:p-6 lg:p-8 space-y-6'>
@@ -54,64 +82,93 @@ const Dashboard = () => {
           View and manage all links at one place.
         </p>
 
-        <div className="w-full flex justify-center">
+        <div className="w-full max-w-3xl flex justify-center mx-auto gap-2 flex flex-col md:flex-row justify-center items-center">
             <input
                 type="text"
+                value={searchParams}
+                onChange={(e)=>setSearchParams(e.target.value)}
                 placeholder="Enter long URL..."
-                className="w-full max-w-3xl px-4 py-3 rounded-lg shadow bg-white border border-gray-300
+                className="w-full px-4 py-3 rounded-lg shadow bg-white border border-gray-300
                 focus:border-blue-500 focus:ring-2 focus:ring-blue-500/50 outline-none "
             />
+            <motion.button 
+                whileHover={{scale:1.05}}
+                whileTap={{scale:0.95}}
+                onClick={()=>navigate('/')}
+                disabled={isLoading}
+                className="w-full md:w-max whitespace-nowrap bg-gradient-to-r from-blue-500 to-indigo-300/60 text-white font-medium px-4 py-3 rounded-md disabled:cursor-not-allowed">
+                Shorten URL
+            </motion.button>
         </div>
 
 
 
+        {isLoading?(
+            <div className='mx-auto flex justify-center items-center bg-white max-w-3xl min-h-70 rounded-lg shadow bg-white'>
+                <Spinner/>
+            </div>
+        ):(
+        <div className="w-full flex justify-center px-4">
+            <div className="w-full max-w-3xl overflow-x-auto rounded-lg shadow bg-white">
+                <Table className="min-w-full ">
+                <TableHeader className="bg-gray-100">
+                    <TableRow>
+                    <TableHead className="w-[140px] text-sm font-semibold text-gray-700">
+                        Shortened URL
+                    </TableHead>
 
-    <div className="w-full flex justify-center px-4">
-        <div className="w-full max-w-3xl overflow-x-auto rounded-lg shadow bg-white">
-            <Table className="min-w-full">
-            <TableHeader className="bg-gray-100">
-                <TableRow>
-                <TableHead className="w-[140px] text-sm font-semibold text-gray-700">
-                    Shortened URL
-                </TableHead>
+                    <TableHead className="text-sm font-semibold text-gray-700">
+                        Long URL
+                    </TableHead>
 
-                <TableHead className="text-sm font-semibold text-gray-700">
-                    Long URL
-                </TableHead>
+                    <TableHead className="text-sm font-semibold text-gray-700 text-center">
+                        Total Clicks
+                    </TableHead>
 
-                <TableHead className="text-sm font-semibold text-gray-700 text-center">
-                    Total Clicks
-                </TableHead>
+                    <TableHead className="text-sm font-semibold text-gray-700 text-center">
+                        Last Used At
+                    </TableHead>
+                    <TableHead className="text-sm font-semibold text-gray-700 text-center">
+                        Actions
+                    </TableHead>
+                    </TableRow>
+                </TableHeader>
 
-                <TableHead className="text-sm font-semibold text-gray-700 text-center">
-                    Last Used At
-                </TableHead>
-                </TableRow>
-            </TableHeader>
+                <TableBody >
+                    {datas.map((data,index)=>(
+                        <TableRow key={data.id} className="hover:bg-gray-50 transition">
+                            <TableCell className="font-medium text-blue-600">
+                                {data.shortCode}
+                            </TableCell>
 
-            <TableBody>
-                <TableRow className="hover:bg-gray-50 transition">
-                <TableCell className="font-medium text-blue-600">
-                    abcdef
-                </TableCell>
+                            <TableCell className="max-w-[250px] overflow-hidden text-ellipsis whitespace-nowrap">
+                                {data.longUrl}
+                            </TableCell>
 
-                <TableCell className="max-w-[250px] overflow-hidden text-ellipsis whitespace-nowrap">
-                    https://www.google.com/search?q=chatgpt+url+shortener+example
-                </TableCell>
+                            <TableCell className="text-center font-semibold">
+                                {data.totalClicks}
+                            </TableCell>
 
-                <TableCell className="text-center font-semibold">
-                    25
-                </TableCell>
+                            <TableCell className="text-center text-gray-600">
+                                {new Date(data.createdAt).toLocaleDateString('en-IN')}
+                            </TableCell>
+                            <TableCell className="flex justify-center text-red-400 hover:text-red-600" 
+                                onClick={()=>{
+                                    handleDeleteData(data.id);
+                                }}
+                            >
+                                <span className='p-2'>
+                                    <Trash2 className='w-4 h-4 '/>
+                                </span>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+                </Table>
 
-                <TableCell className="text-center text-gray-600">
-                    15 Jan 2025
-                </TableCell>
-                </TableRow>
-            </TableBody>
-            </Table>
-
+            </div>
         </div>
-    </div>
+        )}
 
         {/* Pagination and Rows Count */}
         <div className="mx-auto flex flex-col max-w-3xl md:flex-row items-center md:justify-between gap-4 md:items-center md:px-10">
@@ -127,7 +184,7 @@ const Dashboard = () => {
 
           {/* No Of Rows */}
           <div className="w-full md:w-auto flex justify-center items-center md:justify-end gap-3">
-            <span className="font-medium">Results: {Math.min((pageNo-1)*limit+1,totalOrdersCount)} - {Math.min(totalOrdersCount,pageNo*limit)} of {totalOrdersCount}</span>
+            <span className="font-medium">Results: {Math.min((pageNo-1)*limit+1,totalDataCount)} - {Math.min(totalDataCount,pageNo*limit)} of {totalDataCount}</span>
             <select
                 className="border border-gray-300 rounded-md shadow-md px-1 py-1 bg-white"
                 value={limit}
